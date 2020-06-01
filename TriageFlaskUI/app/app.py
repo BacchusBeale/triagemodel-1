96c8cf1fd5f6dd4c71b3751436f6269c7158f296
Bacchus Beale <bealebacchus@gmail.com>
2020-06-01 19:52:43 +1000

    added bootstrap css

--- TriageFlaskUI/app/app.py
+++ TriageFlaskUI/app/app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import render_template, request, redirect
+from flask import render_template, request, redirect, url_for
 from forms import TriageInputForm
 import predictions
 
